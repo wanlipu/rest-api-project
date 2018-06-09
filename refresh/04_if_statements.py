99bb5ace74101f20d254a0d06926bb7f46e63a88
Wanli <puwanli@gmail.com>
2018-06-09 14:15:10 -0500

    edit module 4

---
+++ refresh/04_if_statements.py
@@ -0,0 +1,63 @@
+should_continue = True
+if should_continue:
+    print ("Hello")
+
+know_people = ["John", "Anna", "Mary"]
+person = input("Enter the person you know: ")
+
+if person in know_people:
+    print ("We know {}".format(person))
+else:
+    print ("We don't know {}".format(person))
+
+if person not in know_people:
+    print ("We don't know this person")
+
+
+def who_do_you_know():
+    # Ask the user for a list of people they know
+    # Split the string into a list
+    # Return that list
+    names = input("Please type the list of names of people you know, and separate by space: ")
+    names_list = names.split()
+    return names_list
+
+names = who_do_you_know()
+
+def ask_user(names):
+    # Ask user for a name
+    # See if their name is in the list of people they know
+    # Print out that they know the person
+    name = input("Please type a name you know: ")
+    if name in names:
+        print ("We also know {}".format(name))
+    else:
+        print ("We don't know {}".format(name))
+
+ask_user(names)
+
+
+
+
+
+
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+# Modify the method below to make sure only even numbers are returned.
+def even_numbers():
+    evens = []
+    for number in numbers:
+        if number%2 == 0:
+            evens.append(number)
+    return evens
+
+
+# Modify the below method so that "Quit" is returned if the choice parameter is "q".
+def user_menu(choice):
+    if choice == "q":
+        return "Quit"
+
+print (even_numbers())
+
+
