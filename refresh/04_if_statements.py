9d98dc325fe19a59721e9ed994df9725a79d9dca
Wanli <puwanli@gmail.com>
2018-06-09 18:13:18 -0500

    edit module 4 and add module 5

--- refresh/04_if_statements.py
+++ refresh/04_if_statements.py
@@ -1,17 +1,17 @@
-should_continue = True
-if should_continue:
-    print ("Hello")
-
-know_people = ["John", "Anna", "Mary"]
-person = input("Enter the person you know: ")
-
-if person in know_people:
-    print ("We know {}".format(person))
-else:
-    print ("We don't know {}".format(person))
-
-if person not in know_people:
-    print ("We don't know this person")
+# should_continue = True
+# if should_continue:
+#     print ("Hello")
+#
+# know_people = ["John", "Anna", "Mary"]
+# person = input("Enter the person you know: ")
+#
+# if person in know_people:
+#     print ("We know {}".format(person))
+# else:
+#     print ("We don't know {}".format(person))
+#
+# if person not in know_people:
+#     print ("We don't know this person")
 
 
 def who_do_you_know():
@@ -19,10 +19,15 @@ def who_do_you_know():
     # Split the string into a list
     # Return that list
     names = input("Please type the list of names of people you know, and separate by comma: ")
-    names_list = names.split(",")
-    names_without_spaces = []
-    for name in names_list:
-        names_without_spaces.append(name.strip())
+
+    # names_list = names.split(",")
+    # names_without_spaces = []
+    # for name in names_list:
+    #     names_without_spaces.append(name.strip())
+
+    names_without_spaces = [person.strip() for person in names.split(",")]
+    # names_without_spaces = [person.strip() for person in input("Please type the list of names of people you know, and separate by comma: ").split(",")]
+
     return names_without_spaces
 
 names = who_do_you_know()
@@ -42,26 +47,22 @@ ask_user(names)
 
 
 
-
-
-
-
-numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-
-# Modify the method below to make sure only even numbers are returned.
-def even_numbers():
-    evens = []
-    for number in numbers:
-        if number%2 == 0:
-            evens.append(number)
-    return evens
-
-
-# Modify the below method so that "Quit" is returned if the choice parameter is "q".
-def user_menu(choice):
-    if choice == "q":
-        return "Quit"
-
-print (even_numbers())
+# numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+#
+# # Modify the method below to make sure only even numbers are returned.
+# def even_numbers():
+#     evens = []
+#     for number in numbers:
+#         if number%2 == 0:
+#             evens.append(number)
+#     return evens
+#
+#
+# # Modify the below method so that "Quit" is returned if the choice parameter is "q".
+# def user_menu(choice):
+#     if choice == "q":
+#         return "Quit"
+#
+# print (even_numbers())
 
 
