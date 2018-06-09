d160f14ede715ec01fa6b25d69481b09a20960b5
Wanli <puwanli@gmail.com>
2018-06-09 18:24:32 -0500

    add module 6

---
+++ refresh/06_dictionaries.py
@@ -0,0 +1,30 @@
+my_set = {1, 3, 5}
+print (my_set)
+
+my_dict = {"name": "Jose", "age":90}
+print (my_dict)
+
+lottery_player = {
+    'name': 'Rolf',
+    'numbers': (13, 45, 66, 23, 22)
+}
+
+print (lottery_player)
+print (sum(lottery_player['numbers']))
+lottery_player['name'] = 'John'
+print (lottery_player)
+
+universities = [
+    {
+        'name': 'Oxford',
+        'location': 'UK'
+    },
+    {
+        'name': 'MIT',
+        'location': 'US'
+    }
+]
+
+print (universities)
+
+##
